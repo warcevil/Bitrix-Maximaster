89fda9f091507e039a4042d326b141549bb3e9af
Титов Николай <n.titov@maximaster.ru>
2016-11-07 16:50:11 +0300

    Добавление тестовой платежной системы (интеграция)

---
+++ local/php_interface/include/sale_payment/paymill/.description.php
@@ -0,0 +1,19 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?><?
+$psTitle = "PayMill";
+$psDescription = "";
+
+$arPSCorrespondence = array(
+		"SHOP_CODE" => array(
+				"NAME" => "Shop code",
+				"DESCR" => "The Shop code obtained from the payment system",
+				"VALUE" => "",
+				"TYPE" => ""
+			),
+		"TEST_TRANSACTION" => array(
+				"NAME" => "Test transaction",
+				"DESCR" => "Indicates whether the transaction should be processed as a test transaction (any value)",
+				"VALUE" => "",
+				"TYPE" => ""
+			)
+	);
+?>
