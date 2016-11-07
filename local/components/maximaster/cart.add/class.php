061f74a45b8f1110e43be92fc23880cd3690bcbc
Титов Николай <n.titov@maximaster.ru>
2016-11-07 15:54:21 +0300

    Удаление непонятной строки

--- local/components/maximaster/cart.add/class.php
+++ local/components/maximaster/cart.add/class.php
@@ -16,8 +16,6 @@ class MaximasterCartAjax extends CBitrixComponent
 
     public function onPrepareComponentParams($arParams)
     {
-        $qw = $this->nId;
-
         $arParams['ID'] = intval($arParams['ID']);
         return $arParams;
     }
@@ -25,7 +23,7 @@ class MaximasterCartAjax extends CBitrixComponent
     public function AddProduct() {
         if (CModule::IncludeModule("catalog")) {
             $iProductId = Add2BasketByProductID(
-                $this->arParams['ID'],
+                $this->arParams["ID"],
                 1,
                 array(),
                 array()
